fac589e31023175410f22dfb3fe300a7e498fd54
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2026-05-01 10:28:39 +0200

    add wifi provisioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
-#include <OneWire.h>
 #include <DallasTemperature.h>
+#include <OneWire.h>
+#include "prov.h"
 
 #define ONE_WIRE_GPIO 2
 
@@ -10,22 +11,17 @@ DallasTemperature sensors(&oneWire);
 // put function declarations here:
 int myFunction(int, int);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   sensors.begin();
 
-  Serial.print("Found sensors: ");
-  Serial.println(sensors.getDeviceCount());
+  initWifi();
+
 }
 
-void loop()
-{
+void loop() {
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
-}
+int myFunction(int x, int y) { return x + y; }
